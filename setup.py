eeb1704459b425b7590a929d1e05e21f2127c513
Zwnny <92983587+Zwnny@users.noreply.github.com>
2023-11-30 13:55:57 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,12 @@ from distutils.core import setup
 setup(
     name='fuzzer', version='1.1', description="Python wrapper for multiarch AFL",
     packages=['fuzzer', 'fuzzer.extensions'],
-    data_files = [ ("bin", (os.path.join("bin", "create_dict.py"),)) ],
-    scripts = [ 'shellphuzz' ],
-    install_requires=['angr', 'shellphish-qemu', 'shellphish-afl', 'tqdm']
+    data_files=[("bin", (os.path.join("bin", "create_dict.py"),))],
+    scripts=['shellphuzz'],
+    install_requires=[
+        'angr',
+        'shellphish-qemu',
+        'git+https://github.com/Zwnny/shellphish-afl.git@master',
+        'tqdm'
+    ]
 )
